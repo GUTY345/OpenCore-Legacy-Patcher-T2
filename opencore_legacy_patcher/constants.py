593b019e0d775a00225653b0dd779c97888825cd
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-07-03 00:06:09 +0300

    Update patcher version to 4.0.0prea-161

--- opencore_legacy_patcher/constants.py
+++ opencore_legacy_patcher/constants.py
@@ -13,7 +13,7 @@ from .detections import device_probe
 class Constants:
     def __init__(self) -> None:
         # Patcher Versioning
-        self.patcher_version:                 str = "4.0.0prea-116"  # OpenCore-Legacy-Patcher
+        self.patcher_version:                 str = "4.0.0prea-161"  # OpenCore-Legacy-Patcher
         self.patcher_version_label=self.patcher_version
         self.patcher_support_pkg_version:     str = "1.9.6"  # PatcherSupportPkg
         self.copyright_date:                  str = "Copyright © 2020-2025 Dortania"
