74028139c8ac6a794eb8860a4ca0823f72f47541
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-07-04 18:25:21 +0300

    Remove unused links from constants.py
    Removed Discord and guide links from constants.

--- opencore_legacy_patcher/constants.py
+++ opencore_legacy_patcher/constants.py
@@ -22,11 +22,8 @@ class Constants:
 
         # URLs
         self.url_patcher_support_pkg:         str = "https://github.com/dortania/PatcherSupportPkg/releases/download/"
-        self.discord_link:                    str = "https://discord.gg/rqdPgH8xSN"
-        self.guide_link:                      str = "https://dortania.github.io/OpenCore-Legacy-Patcher/"
         self.repo_link:                       str = "https://github.com/albert-mueller/OpenCore-Legacy-Patcher-T2/"
         self.installer_pkg_url:               str = f"{self.repo_link}/releases/download/{self.patcher_version}/AutoPkg-Assets.pkg"
-        self.installer_pkg_url_nightly:       str = f"{self.repo_link}/releases/download/{self.patcher_version}/AutoPkg-Assets.pkg"
 
         # OpenCore Versioning
         # https://github.com/acidanthera/OpenCorePkg
