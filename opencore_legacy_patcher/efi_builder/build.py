353f211b8683778826d0c2bcac00cf8d770f603b
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-07-12 04:14:27 +0300

    Enhance error logging and update comments in build.py
    Added error logging for function initialization failures and clarified comments regarding Windows Boot Camp installation issues.

--- opencore_legacy_patcher/efi_builder/build.py
+++ opencore_legacy_patcher/efi_builder/build.py
@@ -219,6 +219,7 @@ class BuildOpenCore:
                 logger.exception(f"Fehler, die Funktion, die heißt {function.__name__} zu starten")
                 logger.exception(f"Failed to initialize the function called {function.__name__}")
                 logging.exception("Stack Trace:")
+                sys.exit(3)
 
         # Work-around ocvalidate
         # Auch behebt einen Fehler, indem Windows 10/11 per Boot Camp-Installation verschwindet wegen zu viele Malen \EFI\Microsoft\Boot\bootmgfw.efi erstellt werden oder das \EFI\Microsoft\Boot\bootmgfw.efi erstellen in config.plist, auch wenn es schon da steht.
