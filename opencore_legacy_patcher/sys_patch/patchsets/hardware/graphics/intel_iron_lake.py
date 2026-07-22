3566d940534c591e9e98665e7ea1e16819b339b8
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-07-23 00:01:36 +0300

    Unterstützung für Intel Iron Lake-Grafikkarten für macOS 26 Tahoe verbessern

--- opencore_legacy_patcher/sys_patch/patchsets/hardware/graphics/intel_iron_lake.py
+++ opencore_legacy_patcher/sys_patch/patchsets/hardware/graphics/intel_iron_lake.py
@@ -1,12 +1,15 @@
 """
-intel_iron_lake.py: Intel Iron Lake detection
+intel_ivy_bridge.py: Intel Ivy Bridge detection
 """
 
 from ..base import BaseHardware, HardwareVariant, HardwareVariantGraphicsSubclass
 
-from ...base import PatchType
+from ...base            import PatchType
 
-from ...shared_patches.non_metal       import NonMetal
+from ...shared_patches.metal_3802      import LegacyMetal3802
+from ...shared_patches.big_sur_gva     import BigSurGVA
+from ...shared_patches.monterey_opencl import MontereyOpenCL
+from ...shared_patches.big_sur_opencl  import BigSurOpenCL
 from ...shared_patches.monterey_webkit import MontereyWebKit
 
 from .....constants  import Constants
@@ -15,7 +18,7 @@ from .....detections import device_probe
 from .....datasets.os_data import os_data
 
 
-class IntelIronLake(BaseHardware):
+class IntelIvyBridge(BaseHardware):
 
     def __init__(self, xnu_major, xnu_minor, os_build, global_constants: Constants) -> None:
         super().__init__(xnu_major, xnu_minor, os_build, global_constants)
@@ -25,25 +28,25 @@ class IntelIronLake(BaseHardware):
         """
         Display name for end users
         """
-        return f"{self.hardware_variant()}: Intel Iron Lake"
+        return f"{self.hardware_variant()}: Intel Ivy Bridge"
 
 
     def present(self) -> bool:
         """
-        Targeting Intel Iron Lake GPUs
+        Targeting Intel Ivy Bridge GPUs
         """
         return self._is_gpu_architecture_present(
             gpu_architectures=[
-                device_probe.Intel.Archs.Iron_Lake
+                device_probe.Intel.Archs.Ivy_Bridge
             ]
         )
 
 
     def native_os(self) -> bool:
         """
-        Dropped support with macOS 10.14, Mojave
+        Dropped support with macOS 12, Monterey
         """
-        return self._xnu_major < os_data.mojave.value
+        return self._xnu_major < os_data.monterey.value
 
 
     def hardware_variant(self) -> HardwareVariant:
@@ -57,14 +60,27 @@ class IntelIronLake(BaseHardware):
         """
         Type of hardware variant subclass
         """
-        return HardwareVariantGraphicsSubclass.NON_METAL_GRAPHICS
+        return HardwareVariantGraphicsSubclass.METAL_3802_GRAPHICS
 
 
-    def requires_kernel_debug_kit(self) -> bool:
+    def requires_metallib_support_pkg(self) -> bool:
         """
-        Apple no longer provides standalone kexts in the base OS
+        New compiler format introduced in macOS 15, Sequoia
         """
-        return self._xnu_major >= os_data.ventura.value
+        return self._xnu_major >= os_data.sequoia.value
+
+
+    def _resolve_ivy_bridge_framebuffers(self) -> str:
+        """
+        Resolve patchset directory for Ivy Bridge framebuffers:
+        - AppleIntelFramebufferCapri.kext
+        - AppleIntelHD4000Graphics.kext
+        """
+        if self._xnu_major < os_data.sonoma:
+            return "11.7.10"
+        if self._xnu_float < self.macOS_14_4:
+            return "11.7.10-23"
+        return "11.7.10-23.4"
 
 
     def _model_specific_patches(self) -> dict:
@@ -72,14 +88,16 @@ class IntelIronLake(BaseHardware):
         Model specific patches
         """
         return {
-            "Intel Iron Lake": {
+            "Intel Ivy Bridge": {
                 PatchType.OVERWRITE_SYSTEM_VOLUME: {
                     "/System/Library/Extensions": {
-                        "AppleIntelHDGraphics.kext":           "10.13.6",
-                        "AppleIntelHDGraphicsFB.kext":         "10.13.6",
-                        "AppleIntelHDGraphicsGA.plugin":       "10.13.6",
-                        "AppleIntelHDGraphicsGLDriver.bundle": "10.13.6",
-                        "AppleIntelHDGraphicsVADriver.bundle": "10.13.6",
+                        "AppleIntelHD4000GraphicsGLDriver.bundle":  "11.7.10",
+                        "AppleIntelHD4000GraphicsMTLDriver.bundle": "11.7.10" if self._xnu_major < os_data.ventura else "11.7.10-22",
+                        "AppleIntelHD4000GraphicsVADriver.bundle":  "11.7.10",
+                        "AppleIntelFramebufferCapri.kext":          self._resolve_ivy_bridge_framebuffers(),
+                        "AppleIntelHD4000Graphics.kext":            self._resolve_ivy_bridge_framebuffers(),
+                        "AppleIntelIVBVA.bundle":                   "11.7.10",
+                        "AppleIntelGraphicsShared.bundle":          "11.7.10", # libIGIL-Metal.dylib pulled from 11.0 Beta 6
                     },
                 },
             },
@@ -88,16 +106,16 @@ class IntelIronLake(BaseHardware):
 
     def patches(self) -> dict:
         """
-        Patches for Intel Iron Lake iGPUs
+        Patches for Intel Ivy Bridge iGPUs
         """
         if self.native_os() is True:
             return {}
 
-        if self._xnu_major not in self._constants.legacy_accel_support and self._dortania_internal_check() is False:
-            return {**self._model_specific_patches()}
-
         return {
-            **NonMetal(self._xnu_major, self._xnu_minor, self._os_build).patches(),
+            **LegacyMetal3802(self._xnu_major, self._xnu_minor, self._constants.detected_os_version).patches(),
+            **BigSurGVA(self._xnu_major, self._xnu_minor, self._constants.detected_os_version).patches(),
+            **MontereyOpenCL(self._xnu_major, self._xnu_minor, self._constants.detected_os_version).patches(),
+            **BigSurOpenCL(self._xnu_major, self._xnu_minor, self._constants.detected_os_version).patches(),
             **MontereyWebKit(self._xnu_major, self._xnu_minor, self._os_build).patches(),
             **self._model_specific_patches(),
         }
