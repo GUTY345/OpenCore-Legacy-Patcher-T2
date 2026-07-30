10a4fec331a2dfea5046c9165e5f2fecd619f7b7
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-07-30 21:39:59 +0300

    Update gui_settings.py

--- opencore_legacy_patcher/wx_gui/gui_settings.py
+++ opencore_legacy_patcher/wx_gui/gui_settings.py
@@ -811,7 +811,8 @@ class SettingsFrame(wx.Frame):
                     ],
                     "warning": "This option should only be used if your Mac natively supports the OSes you wish to run.\n\nIf you are currently running an unsupported OS, this option will break booting. Only toggle for enabling OS features on a native Mac.\n\nAre you certain you want to continue?",
                 },
-                "Disable automatic updates": {
+                # um zu sicherstellen, dass Benutzer auf den neuesten Stand bleiben, um die letzte Fehlerbehebungen zu erhalten und Sicherheitslücken zu schließen, dieses Menü ist standardmäßig deaktiviert
+                # "Disable automatic updates": {
                     "type": "checkbox",
                     "value": global_settings.GlobalEnviromentSettings().read_property("IgnoreAppUpdates") or self.constants.ignore_updates,
                     "variable": "IgnoreAppUpdates",
