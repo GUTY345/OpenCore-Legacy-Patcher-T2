6d1525d3677292619087e6f61e1b8620798ab07a
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-08-06 04:32:04 +0300

    macOS 26-Unterstützung für NVIDIA Tesla verbessern

--- opencore_legacy_patcher/sys_patch/patchsets/hardware/graphics/nvidia_tesla.py
+++ opencore_legacy_patcher/sys_patch/patchsets/hardware/graphics/nvidia_tesla.py
@@ -25,7 +25,7 @@ class NvidiaTesla(BaseHardware):
         """
         Display name for end users
         """
-        return f"{self.hardware_variant()}: Nvidia Tesla"
+        return f"{self._trans.get(self.hardware_variant(), self.hardware_variant())}: {self._trans.get('Nvidia Tesla', 'Nvidia Tesla')}"
 
 
     def present(self) -> bool:
@@ -43,7 +43,7 @@ class NvidiaTesla(BaseHardware):
         """
         Dropped support with macOS 10.14, Mojave
         """
-        return self._xnu_major < os_data.mojave.value
+        return self._xnu_major < os_data.mojave.value or self._xnu_major >= os_data.tahoe.value
 
 
     def hardware_variant(self) -> HardwareVariant:
@@ -96,7 +96,7 @@ class NvidiaTesla(BaseHardware):
         if self.native_os() is True:
             return {}
 
-        if self._xnu_major not in self._constants.legacy_accel_support and self._dortania_internal_check() is False:
+        if self._xnu_major not in self._constants.legacy_accel_support and self._hackdoc_internal_check() is False:
             return {**self._model_specific_patches()}
 
         return {
