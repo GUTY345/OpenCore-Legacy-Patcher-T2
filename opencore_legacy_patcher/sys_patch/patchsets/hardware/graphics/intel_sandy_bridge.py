ce344c8337f873d41ca79103c8033176b2b01af8
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-08-06 04:28:59 +0300

    macOS 26-Unterstützung auf Intel Sandy Bridge-Grafikkarten verbessern

--- opencore_legacy_patcher/sys_patch/patchsets/hardware/graphics/intel_sandy_bridge.py
+++ opencore_legacy_patcher/sys_patch/patchsets/hardware/graphics/intel_sandy_bridge.py
@@ -26,7 +26,7 @@ class IntelSandyBridge(BaseHardware):
         """
         Display name for end users
         """
-        return f"{self.hardware_variant()}: Intel Sandy Bridge"
+        return f"{self._trans.get(self.hardware_variant(), self.hardware_variant())}: {self._trans.get('Intel Sandy Bridge', 'Intel Sandy Bridge')}"
 
 
     def present(self) -> bool:
@@ -44,7 +44,7 @@ class IntelSandyBridge(BaseHardware):
         """
         Dropped support with macOS 10.14, Mojave
         """
-        return self._xnu_major < os_data.mojave.value
+        return self._xnu_major < os_data.mojave.value or self._xnu_major >= os_data.tahoe.value
 
 
     def hardware_variant(self) -> HardwareVariant:
@@ -97,7 +97,7 @@ class IntelSandyBridge(BaseHardware):
         if self.native_os() is True:
             return {}
 
-        if self._xnu_major not in self._constants.legacy_accel_support and self._dortania_internal_check() is False:
+        if self._xnu_major not in self._constants.legacy_accel_support and self._hackdoc_internal_check() is False:
             return {**self._model_specific_patches()}
 
         return {
