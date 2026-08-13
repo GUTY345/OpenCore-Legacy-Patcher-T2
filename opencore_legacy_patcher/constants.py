cca37c3697b5f2fa37317192639f168f0c2dec81
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-08-13 12:02:36 +0300

    Update OpenCore version to 2.0.0.2

--- opencore_legacy_patcher/constants.py
+++ opencore_legacy_patcher/constants.py
@@ -31,7 +31,7 @@ class Constants:
 
         # OpenCore Versioning
         # https://github.com/acidanthera/OpenCorePkg
-        self.opencore_version: str = "1.0.7"
+        self.opencore_version: str = "2.0.0.2"
 
         # Kext Versioning
         ## Acidanthera
