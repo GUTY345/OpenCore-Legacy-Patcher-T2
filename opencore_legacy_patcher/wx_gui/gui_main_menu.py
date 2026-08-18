573d55eafce04bf9f6d89e0cb2676e4d86fd2260
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-08-18 16:13:45 +0300

    Fix: Update-Dialog-Text wird bei fester Dialogbreite abgeschnitten (Wrap hinzugefügt)

--- opencore_legacy_patcher/wx_gui/gui_main_menu.py
+++ opencore_legacy_patcher/wx_gui/gui_main_menu.py
@@ -297,6 +297,9 @@ class MainFrame(wx.Frame):
         self.description = wx.StaticText(panel, label=f"{self.constants.patcher_name} {oclp_version} is now available - You have {self.constants.patcher_version_label}. Would you like to update?")
         self.title_text.SetFont(gui_support.font_factory(19, wx.FONTWEIGHT_BOLD))
         self.description.SetFont(gui_support.font_factory(13, wx.FONTWEIGHT_NORMAL))
+        # Ohne Wrap() ragt der Text bei langen Versions-/Produktnamen über die feste Dialogbreite hinaus
+        # und wird dadurch abgeschnitten (z.B. "Would you like to update?" -> "Would you like to").
+        self.description.Wrap(600)
         
         self.web_view = wx.html2.WebView.New(panel, style=wx.BORDER_SUNKEN)
         html_code = f'''
