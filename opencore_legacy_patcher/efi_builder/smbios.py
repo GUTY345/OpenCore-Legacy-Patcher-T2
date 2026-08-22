528f2558ddf923c84594ecc3af696f857b3df59f
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-08-22 21:42:56 +0300

    smbios: skip AppleSMC/SMC-Spoof exemption patch on T2 Macs (causes kernel panic)

--- opencore_legacy_patcher/efi_builder/smbios.py
+++ opencore_legacy_patcher/efi_builder/smbios.py
@@ -59,22 +59,20 @@ class BuildSMBIOS:
                     logging.info("Please try again later.")
                     sys.exit(3)
 
+        elif self.model in model_array.T2Macs:
+            # com.apple.driver.AppleSMC causes kernel panics on T2 Macs; only inject the SMC exemption patch on non-T2 Macs
+            logging.info("- Skipping SMC exemption patch (T2 Mac detected, avoids AppleSMC kernel panic)")
+
         else:
-            # com.apple.driver.AppleSMC (+ SMC-Spoof.kext) causes kernel panics on T2
-            # Macs, so only inject this exemption patch on Macs without a T2 chip.
-            is_t2_mac = self.model in model_array.T2Macs or "T2_CHIP" in self.constants.device_properties.get(self.model, {}).get("Features", [])
-            if is_t2_mac:
-                logging.info("- Skipping SMC exemption patch: com.apple.driver.AppleSMC and SMC-Spoof.kext cause kernel panics on T2 Macs")
-            else:
-                try:
-                    logging.info("- Enabling SMC exemption patch")
-                    support.BuildSupport(self.model, self.constants, self.config).get_item_by_kv(self.config["Kernel"]["Patch"], "Identifier", "com.apple.driver.AppleSMC")["Enabled"] = True
-                    support.BuildSupport(self.model, self.constants, self.config).enable_kext("SMC-Spoof.kext", self.constants.smcspoof_version, self.constants.smcspoof_path)
-                except Exception as E:
-                    logging.error("Whoops, injecting SMC-Spoof.kext failed because of the following error:")
-                    logging.exception("Stack Trace:") # This prints the full technical error
-                    logging.info("Please try again later.")
-                    sys.exit(3)
+            try:
+                logging.info("- Enabling SMC exemption patch")
+                support.BuildSupport(self.model, self.constants, self.config).get_item_by_kv(self.config["Kernel"]["Patch"], "Identifier", "com.apple.driver.AppleSMC")["Enabled"] = True
+                support.BuildSupport(self.model, self.constants, self.config).enable_kext("SMC-Spoof.kext", self.constants.smcspoof_version, self.constants.smcspoof_path)
+            except Exception as E:
+                logging.error("Whoops, injecting SMC-Spoof.kext failed because of the following error:")
+                logging.exception("Stack Trace:") # This prints the full technical error
+                logging.info("Please try again later.")
+                sys.exit(3)
 
         if self.constants.serial_settings in ["Moderate", "Advanced"]:
             try:
