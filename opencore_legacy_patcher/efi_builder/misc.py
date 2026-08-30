085471d31f7edee350c774a00ebcb2ea9e0ba5a1
Albert Müller <p8bpg9zrw7@privaterelay.appleid.com>
2026-08-29 19:06:19 -0700

    Fix IndentationError: extra space before try: in _t2_handling (efi_builder/misc.py)
    The try: block for disabling UEFI updates on T2 Macs had 17 spaces of indentation instead of the 16 used everywhere else in this block, which made Python treat it as an unindent to a nonexistent level and crashed on import with "IndentationError: unindent does not match any outer indentation level".

--- opencore_legacy_patcher/efi_builder/misc.py
+++ opencore_legacy_patcher/efi_builder/misc.py
@@ -654,7 +654,7 @@ class BuildMiscellaneous:
                     logging.info("Please try again later.")
                     sys.exit(3)
                 # dieses Patch bringt dazu, dass T2 Macs den Fehler nach 29 Minuten verbleibend weiter mit die Installation fährt statt einen Fehler zu zeigen
-                 try:
+                try:
                     logging.info("- Disabling UEFI updates for T2 Macs to prevent errors after 29 minutes remaining")
                     logging.info("This patch will block UEFI updates on T2 Macs. To update the UEFI on T2 Macs while running unsupported macOS versions, if you care about your UEFI being up to date, you'll need to update it via exiting OpenCore and entering DFU mode using another Mac.")
                     self._update_nvram_string(APPLE_NVRAM_UUID, "boot-args", "-oas_skip_attestation")
