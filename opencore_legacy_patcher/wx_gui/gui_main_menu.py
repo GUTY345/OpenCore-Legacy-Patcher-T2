b8b16335a038e88e483d3499620fb24f542a230d
Medelcartelinc <matteoiaccarino97@icloud.com>
2026-08-30 05:58:46 +0200

    Restore missing Developer Mode buttons

--- opencore_legacy_patcher/wx_gui/gui_main_menu.py
+++ opencore_legacy_patcher/wx_gui/gui_main_menu.py
@@ -125,12 +125,18 @@ class MainFrame(wx.Frame):
                     "description": ["Download and flash a macOS", "Installer for your system."],
                     "icon": str(self.constants.icns_resource_path / "OC-Installer.icns"),
                 },
+                "Install drivers and patches": {
+                    "function": self.on_root_patches,
+                    "description": ["Installs hardware drivers and", "patches for your system after", "installing a new version of macOS."],
+                    "icon": str(self.constants.patch_icon_path),
+                    "info_tab": 2,
+                },
                 "macOS Configuration": {
                     "function": self.on_macos_config,
                     "description": ["Settings, drivers and", "patches for your system."],
                     "icon": str(self.constants.patch_icon_path),
                 },
-                "OpenCore": {
+                "OpenCore Settings": {
                     "function": self.on_oc_settings,
                     "description": ["Prepares provided drive to be", "able to boot unsupported OSes."],
                     "icon": str(self.constants.icns_resource_path / "OC-Build.icns"),
